aabe67ed58a107ba287e266911cb20fd8a511cc3
Logeshwaran S S <logesh0781@gmail.com>
2026-08-05 16:29:20 +0530

    Time: 6 ms (42.98%), Space: 44.1 MB (95.44%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,18 +3,18 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        HashMap <Character,Integer> f=new HashMap<>();
-        char[] sc=s.toCharArray();
-        char[] tc=t.toCharArray();
-        for(char a:sc){
-            f.put(a,f.getOrDefault(a,0)+1);
+       int[] freq = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
         }
-        for(char a:tc){
-            f.put(a,f.getOrDefault(a,0)-1);
+        
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] != 0) {
+                return false;
+            }
         }
-        for(int i:f.values()){
-            if(i!=0) return false;
-        }
+        
         return true;
     }
 }
